f4999d48f5c6c939648f2f96c81cd586ecb5ad5a
tomaskom <tomaskom@gmail.com>
2025-02-01 16:57:09 -0800

    fixed port for deployment

--- server/server.js
+++ server/server.js
@@ -60,7 +60,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
