fc847e9016140e01ec278b5b9f4319082cd6ced3
tomaskom <tomaskom@gmail.com>
2025-02-01 20:13:07 -0800

    fix the port number for local tests

--- server/server.js
+++ server/server.js
@@ -60,7 +60,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
